5711d6368f2314aedeca4074785552897e88bff8
Deepjune <wjdwnsrn9021@naver.com>
2023-09-10 23:27:35 +0900

    Email

---
+++ Spring/emailCertified/src/main/java/emailCertified/emailCertified/EmailCertifiedApplication.java
@@ -0,0 +1,13 @@
+package emailCertified.emailCertified;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailCertifiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailCertifiedApplication.class, args);
+	}
+
+}
---
+++ Spring/emailCertified/src/test/java/emailCertified/emailCertified/EmailCertifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package emailCertified.emailCertified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailCertifiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
